db0b38649544a2a2a24bc89894808df8ea01f347
kushal saini <saini.kushal2@gmail.com>
2021-01-01 20:53:32 +0530

    header in center using transform translate.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import logoWhite from "./assets/logo-white.png"; // Tell webpack this JS file uses this image
 
 function App() {
-  return <header className="header">Some Text ...</header>;
+    return (
+        <header className="header">
+            <div className="logo-box">
+                <img className="logo" alt="logo" src={logoWhite} />
+            </div>
+            <div className="text-box">
+                <h1 className="primary-heading">
+                    <span className="primary-heading-main">Outdoors</span>
+                    <span className="primary-heading-sub">
+                        is where life happens
+                    </span>
+                </h1>
+            </div>
+        </header>
+    );
 }
 
 export default App;
