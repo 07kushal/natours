c2b4d120563e6a1791779a0c26704c1b3e92aa58
kushal saini <saini.kushal2@gmail.com>
2021-01-02 18:29:29 +0530

    css converted into BEM(block element modifier)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import logoWhite from "./assets/logo-white.png"; // Tell webpack this JS file us
 function App() {
     return (
         <header className="header">
-            <div className="logo-box">
-                <img className="logo" alt="logo" src={logoWhite} />
+            <div className="header__logo-box">
+                <img className="header__logo" alt="logo" src={logoWhite} />
             </div>
-            <div className="text-box">
+            <div className="header__text-box">
                 <h1 className="primary-heading">
-                    <span className="primary-heading-main">Outdoors</span>
-                    <span className="primary-heading-sub">
+                    <span className="primary-heading--main">Outdoors</span>
+                    <span className="primary-heading--sub">
                         is where life happens
                     </span>
                 </h1>
-                <a href="#" className="btn btn-white btn-animated">
+                <a href="#" className="btn btn--white btn--animated">
                     Discover our tours
                 </a>
             </div>
