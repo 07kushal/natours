edb4c0f430b0679bb39f0fc745f438eb05b40752
kushal saini <saini.kushal2@gmail.com>
2021-01-17 19:38:55 +0530

    card added front and back

--- src/App.js
+++ src/App.js
@@ -133,6 +133,45 @@ function App() {
                         </div>
                     </div>
                 </section>
+                <section className="section-tour">
+                    <div className="u-center-text u-margin-bottom-big">
+                        <h2 className="heading-secondary">
+                            Most popular tours
+                        </h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-1-of-3">
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    FRONT
+                                </div>
+                                <div className="card__side card__side--back card__side--back-1">
+                                    BACK
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1-of-3">
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    FRONT
+                                </div>
+                                <div className="card__side card__side--back card__side--back-2">
+                                    BACK
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1-of-3">
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    FRONT
+                                </div>
+                                <div className="card__side card__side--back card__side--back-3">
+                                    BACK
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 {/* <section>
                     <div className="grid-test">
                         <div className="row">
